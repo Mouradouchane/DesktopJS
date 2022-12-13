f8de0380c4cf4f2678f62666da93ce5d22cd81d4
Mouradouchane <mouradouchane@gmail.com>
2022-12-13 07:08:47 +0100

    progress:in class element

--- Desktop-dev/element.js
+++ Desktop-dev/element.js
@@ -30,18 +30,94 @@ export class element {
     */
     #callbacks = {
 
+        set_callback : ( event_name , call_back_function , ...call_back_args ) => {
+
+            if( typeof( call_back_function ) === "function" ){
+
+                this.#callbacks[event_name].call_back_function = call_back_function;
+                this.#callbacks[event_name].call_back_args = call_back_args;
+
+                return true;
+            }
+            else return false;
+            
+        },
+
     };
 
     // on event
     on = {
 
-        click : ( call_back_function , ...call_back_args ) => { },
-        click_right : ( call_back_function , ...call_back_args ) => { },
+        click : ( call_back_function , ...call_back_args ) => { 
 
-        dblclick : ( call_back_function , ...call_back_args ) => { },
-        dblclick_right : ( call_back_function , ...call_back_args ) => { },
+           if( this.#callbacks.set_callback("click", call_back_function , ...call_back_args) ){
+
+                this.#props.html.addEventListener("click" , (event) => {
+                    this.#callbacks.click.call_back_function( this , event , ...( this.#callbacks.click.call_back_args));
+                });
+
+           }
+           else // need error here
+           console.error("on.click :", "parameter 'call_back_function' must to be function"); 
+           
+        },
+        click_right : ( call_back_function , ...call_back_args ) => { 
+
+            if( this.#callbacks.set_callback("click_right", call_back_function , ...call_back_args) ){
+
+                this.#props.html.addEventListener("contextmenu" , (event) => {
+                    this.#callbacks.click_right.call_back_function( this , event , ...( this.#callbacks.click_right.call_back_args));
+                });
+
+            }
+            else // need error here
+            console.error("on.click_right :", "parameter 'call_back_function' must to be function");
         
-        hover : ( call_back_function , ...call_back_args ) => { },
+        },
+
+        click_down : ( call_back_function , ...call_back_args ) => { 
+
+            if( this.#callbacks.set_callback("click_down", call_back_function , ...call_back_args) ){
+
+                this.#props.html.addEventListener("mousedown" , (event) => {
+                    this.#callbacks.click_down.call_back_function( this , event , ...( this.#callbacks.click_down.call_back_args));
+                });
+
+            }
+            else // need error here
+            console.error("on.click_down :", "parameter 'call_back_function' must to be function");
+        
+        },
+
+        dblclick : ( call_back_function , ...call_back_args ) => { 
+
+            if( this.#callbacks.set_callback("dblclick", call_back_function , ...call_back_args) ){
+
+                this.#props.html.addEventListener("dblclick" , (event) => {
+                    this.#callbacks.dblclick.call_back_function( this , event , ...( this.#callbacks.dblclick.call_back_args));
+                });
+
+            }
+            else // need error here
+            console.error("on.dblclick :", "parameter 'call_back_function' must to be function");
+        
+        },
+
+        hover : ( call_back_function , ...call_back_args ) => { 
+  
+            if( this.#callbacks.set_callback("hover", call_back_function , ...call_back_args) ){
+
+                this.#props.html.addEventListener("mouseover" , (event) => {
+
+                    this.#callbacks.hover.call_back_function( this , event , ...( this.#callbacks.hover.call_back_args));
+                
+                });
+
+            }
+            else // need error here
+            console.error("on.hover :", "parameter 'call_back_function' must to be function");
+
+        },
 
         resize : ( call_back_function , ...call_back_args ) => { },
         drag_start : ( call_back_function , ...call_back_args ) => { },
@@ -64,21 +140,66 @@ export class element {
 
     set = {
 
+        // we need => html , css 
     };
 
+
+    add = {
+
+        parent : ( new_parent = new element() ) => {
+        
+            if( new_parent instanceof element ){
+
+                this.#props.parents += new_parent;
+                new_parent.add.child(this);
+
+            }
+                 // need error
+            else console.error("add.parent :" , "parameter 'new_parent' must to be a instance of 'class element'"); 
+        },
+
+        child : ( new_child = new element() ) => {
+            
+            if( new_child instanceof element ){
+
+                this.#props.children += new_child;
+                this.#props.html.appendChild(new_child.#props.html);
+
+            }
+                 // need error
+            else console.error("add.child :" , "parameter 'new_child' must to be a instance of 'class element'"); 
+        
+        }
+
+    }
 
     constructor(
         id = null , title = "" , text = "" , x = 0 , y = 0 , width = 0 , height = 0 , 
         visible = true , draggable = false , resizable = false  
     ){
 
+        // save values 
 
-        // set callbacks list to default  
+        this.#props.id    = (typeof(id)    == "string") ? id : undefined;
+        this.#props.title = (typeof(title) == "string") ? title : undefined;
+        this.#props.text  = (typeof(text)  == "string") ? text : "";
+
+        this.#props.x = (typeof(x) == "number") ? x : 0;
+        this.#props.y = (typeof(y) == "number") ? y : 0;
+
+        this.#props.width  = (typeof(width)  == "number") ? width : 0;
+        this.#props.height = (typeof(height) == "number") ? height : 0;
+
+        this.#props.visible   = (typeof(visible)   == "boolean") ? visible : false;
+        this.#props.draggable = (typeof(draggable) == "boolean") ? draggable : false;
+        this.#props.resizable = (typeof(resizable) == "boolean") ? resizable : false;
+        
+
+        // set callbacks to default  
 
         const events_names = [
-            "click","click_right","dblclick","dblclick_right", 
-            "hover","resize","drag_start","drag_in","drag_end",
-            "visible", "hidden", "delete", "css_change", "html_change" 
+            "click","click_right","dblclick","hover","resize","drag_start","drag_in",
+            "drag_end","visible", "hidden", "delete", "css_change", "html_change","click_down"
         ];
 
         for( const name of events_names ){
@@ -89,21 +210,56 @@ export class element {
         }
 
 
-        // set getter's list 
+        // setup getter's functions 
 
-        const getters_functions_names = [
+        const getters_functions = [
             "title","id","text","x","y","old_x","old_y",
             "width","old_width","height","old_height",
-            "css","resizable","draggable","visible","html",
-            "index"
+            "resizable","draggable","visible","index"
         ];
 
-        for( const name of getters_functions_names ){
-            return this.#props[name];
+        for( const name of getters_functions ){
+
+            this.get[name] = () => {
+                return this.#props[name];
+            }
+
         }
 
 
+        // setup setter's functions 
+
+        const setters_functions = [
+           // function name , parameter type
+            {name : "title" , type : "string"},
+            {name : "text"  , type : "string"},
+            {name : "x"     , type : "number"},
+            {name : "y"     , type : "number"},
+            {name : "width" , type : "number"},
+            {name : "height", type : "number"},
+            {name : "index" , type : "number"},
+            {name : "visible", type : "boolean"},
+            {name : "draggable", type : "boolean"},
+            {name : "resizable", type : "boolean"},
+        ];
+
+        for( const fn of setters_functions ){
+
+            this.set[fn.name] = ( new_value ) => {
+                
+                if( typeof(new_value) != fn.type ) console.warn( "set."+fn.name ,": parameter must to be =>" , fn.type ); // need warn from model
+                else this.#props[fn.name] = new_value;
+
+            } 
+
+        }
+
+        // just for test !!!
+        this.#props.html.textContent = text;
+        this.#props.html.id = id;
+        document.body.append(this.#props.html);
 
     } // end of constructor
+
 
 }
